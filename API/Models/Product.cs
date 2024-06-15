a6d5d0cbab0464a2a00fd6040c8c68af8c39cc12
nrphuoc <nguyentuongphuoc@gmail.com>
2024-06-15 08:35:57 +0700

    add appointment

--- API/Models/Product.cs
+++ API/Models/Product.cs
@@ -7,7 +7,14 @@ namespace API.Models
     {
         public Product()
         {
+<<<<<<< HEAD
             SaleItems = new HashSet<SaleItem>();
+=======
+<<<<<<< HEAD
+            SaleItems = new HashSet<SaleItem>();
+=======
+>>>>>>> 3c01c19a3da51403c8e5862e6ab2cada4a6bd574
+>>>>>>> 80d94dc5e6631bb94fe479a2e337b265a77ebfa9
             Categories = new HashSet<Category>();
         }
 
@@ -18,9 +25,18 @@ namespace API.Models
         public decimal? Price { get; set; }
         public int? QuantityInStock { get; set; }
 
+<<<<<<< HEAD
         public virtual Spa? Spa { get; set; }
         public virtual ICollection<SaleItem> SaleItems { get; set; }
 
+=======
+<<<<<<< HEAD
+        public virtual Spa? Spa { get; set; }
+        public virtual ICollection<SaleItem> SaleItems { get; set; }
+
+=======
+>>>>>>> 3c01c19a3da51403c8e5862e6ab2cada4a6bd574
+>>>>>>> 80d94dc5e6631bb94fe479a2e337b265a77ebfa9
         public virtual ICollection<Category> Categories { get; set; }
     }
 }
--- API/Models/Promotion.cs
+++ API/Models/Promotion.cs
@@ -12,7 +12,15 @@ namespace API.Models
         public DateTime? EndDate { get; set; }
         public string? Description { get; set; }
         public decimal? DiscountPercentage { get; set; }
+<<<<<<< HEAD
 
         public virtual Spa? Spa { get; set; }
+=======
+<<<<<<< HEAD
+
+        public virtual Spa? Spa { get; set; }
+=======
+>>>>>>> 3c01c19a3da51403c8e5862e6ab2cada4a6bd574
+>>>>>>> 80d94dc5e6631bb94fe479a2e337b265a77ebfa9
     }
 }
