f04d0ba6cb7a212ad33525aa7d0c4d404aeb8058
TruongNQHE140426 <truongnqhe140426@fpt.edu.vn>
2024-06-17 01:34:11 +0700

    feat: add base auth with role

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using API.Dtos;
+using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IUserService _userService;
+        private readonly IConfiguration _configuration;
+        public AuthController(IUserService userService, IConfiguration configuration)
+        {
+            _userService = userService;
+            _configuration = configuration;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequest model)
+        {
+            var user = await _userService.Authenticate(model.Username, model.Password);
+
+            if (user == null)
+                return Unauthorized();
+
+            var tokenString = GenerateJwtToken(user);
+            return Ok(new { Token = tokenString });
+        }
+
+        private string GenerateJwtToken(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(30),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ API/README.md
@@ -0,0 +1,22 @@
+﻿### Cách gọi để lấy dữ liệu theo ROLE
+
+Các policy của các ROLE: RequireCEO, RequireManager, RequireReceptions, RequireStaff
+
+[HttpGet("ceo")]
+[Authorize(Policy = "RequireCEO")]
+public IActionResult GetCEOData()
+{
+    // Code để lấy dữ liệu chỉ dành cho CEO
+    return Ok(new { Message = "This data is only accessible to CEO." });
+}
+
+[HttpGet("manager")]
+[Authorize(Policy = "RequireManager")]
+public IActionResult GetManagerData()
+{
+    // Code để lấy dữ liệu chỉ dành cho Manager
+    return Ok(new { Message = "This data is only accessible to Managers." });
+}
+
+Các ROLE còn lại tương tự
+.....
