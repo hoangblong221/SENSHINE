0d05a55337c772a159b1365de588500c61f72b8c
Kytade160671 <kytade160671@fpt.edu.vn>
2024-06-17 22:14:24 +0700

    HTML CSS HOMEPAGE

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace Web.Controllers
         {
             return View();
         }
+        public IActionResult HomePage()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
