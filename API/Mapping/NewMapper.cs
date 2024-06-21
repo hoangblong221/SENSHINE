3c8d0979211e35ce108002e29a7e4bf97c033eb6
DuyNNHE150074 <duynnhe150074@fpt.edu.vn>
2024-06-22 00:17:29 +0700

    fix: fix all mappers

--- API/Mapping/NewMapper.cs
+++ API/Mapping/NewMapper.cs
@@ -7,14 +7,10 @@ namespace API.Mapping
     public class NewMapper : Profile
     {
         public NewMapper()
-        {   //News Automapper
+        {
             CreateMap<News, NewsDTO>().ReverseMap();
             CreateMap<News, NewsDTORequest>()
                 .ForMember(dest => dest.PublishedDate, opt => opt.MapFrom(src => src.PublishedDate.ToString("yyyy/MM/dd"))).ReverseMap();
-
-            //Promotions Automapper
-            CreateMap<Promotion, PromotionDTO>().ReverseMap();
-            CreateMap<Promotion, PromotionDTORequest>().ReverseMap();
         }
     }
 }
