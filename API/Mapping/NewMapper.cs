9a29e95b893939adf7b6ee673b42044af2c0ffad
DuyNNHE150074 <duynnhe150074@fpt.edu.vn>
2024-06-22 00:14:04 +0700

    fix: fix mapper class

--- API/Mapping/NewMapper.cs
+++ API/Mapping/NewMapper.cs
@@ -15,14 +15,6 @@ namespace API.Mapping
             //Promotions Automapper
             CreateMap<Promotion, PromotionDTO>().ReverseMap();
             CreateMap<Promotion, PromotionDTORequest>().ReverseMap();
-
-            //Card Automapper
-            CreateMap<Card, CardDTO>()
-                .ForMember(dest => dest.CustomerName, opt => opt.MapFrom(src => src.Customer.FirstName + " " + src.Customer.MidName + " " + src.Customer.LastName))
-                .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Customer.Phone))
-                .ForMember(dest => dest.ComboName, opt => opt.MapFrom(src => src.Combos.Select(s => s.Name).ToList()))
-                .ForMember(dest => dest.InvoiceId, opt => opt.MapFrom(src => src.Invoices.Select(s => s.Id).ToList()));
-            CreateMap<CardDTO, Card>();
         }
     }
 }
