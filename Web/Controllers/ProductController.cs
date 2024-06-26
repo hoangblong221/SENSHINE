9c0a9087966f961a27db931d94ac4da85214416b
Kytade160671 <kytade160671@fpt.edu.vn>
2024-06-26 15:16:11 +0700

    feat: Make controller Product , Category for list product

---
+++ Web/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        Uri baseAddress = new Uri("http://localhost:5297/api");
+        private readonly HttpClient _httpClient;
+
+        public ProductController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = baseAddress;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductList()
+        {
+            List<ProductViewModel> viewList = new List<ProductViewModel>();
+            HttpResponseMessage response = await _httpClient.GetAsync(_httpClient.BaseAddress + "/ListAllProducts");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                viewList = JsonConvert.DeserializeObject<List<ProductViewModel>>(data);
+            }
+
+            ViewData["Title"] = "List Products";
+            return View(viewList);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            List<CategoryViewModel> viewList = new List<CategoryViewModel>();
+            HttpResponseMessage response = await _httpClient.GetAsync(_httpClient.BaseAddress + "/ListAllCategories");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                viewList = JsonConvert.DeserializeObject<List<CategoryViewModel>>(data);
+            }
+
+
+            return View(viewList);
+        }
+    }
+}
---
+++ Web/Models/CategoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.Models
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; } = null!;
+    }
+}
